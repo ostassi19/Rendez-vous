3cc886e1b5f6385d90ac852b7ae6309181736bf0
Amani Miled <57594886+ostassi19@users.noreply.github.com>
2021-02-23 22:06:27 +0100

    rendez vous de joue

--- src/Controller/medecinController.php
+++ src/Controller/medecinController.php
@@ -13,27 +13,31 @@ class medecinController extends Controller
     /**
      * @Route("medecin", name="medecin-accueil")
      */
-    public function base (){
+    public function base()
+    {
 
         $id = $this->getUser()->getIdPersonne();
         $em = $this->getDoctrine()->getManager();
         $medecin = $em->getRepository('App:Medecin')->find($id);
-        //cherche si le medecin connecté se trouve dans la base médecin
+        /*cherche si le medecin connecté se trouve dans la base médecin
         if ($medecin instanceof Medecin){
             // liste des rendez-vous d'un medecin
             $rvs = new ArrayCollection();
             // liste des fiches d'un medecin
-            foreach ($medecin->getFiches() as $key => $fiche){
+            foreach ($medecin->getFiches() as $key => $fiche)
                 //liste des rendez_vous dans un fiche
                 foreach ($fiche->getRendezVouses() as $rv)
                     //ajouter les rendez-vous d'un fiche donnée dans la table de type arrayCollection
                     $rvs->add($rv);
             }
-        }
+        }*/
+        $d = $em->getRepository('App:Date')->findOneBy(['date' => new \DateTime(), 'medecin' => $this->getUser()->getIdPersonne()]);
+
+        $rvs = null;
         //la fonction va etre applicable dans base-medecin.html.twig en récupérant deux variables (Medecin, rvs)
         return $this->render('base_medecin.html.twig', array(
             'Medecin' => $medecin,
-            'rvs' => $rvs
+            'rvs' => $d->getRendezVouses()
         ));
     }
 }
